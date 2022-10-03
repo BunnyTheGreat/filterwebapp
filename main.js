f7c7dfe1edba82a712e83bad4d0f16f8ef9fce85
BunnyTheGreat <81476131+BunnyTheGreat@users.noreply.github.com>
2022-10-02 20:42:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 function preload() {
-
+lips = loadImage("https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/77135/lip-clipart-md.png")
 }
 
+lips_x = 0;
+lips_y = 0;
+
 function takeSnapshot() {
-    save('clownnosefilter.png');
+    save('lipstickfilter.png');
 }
 
 function setup() {
 canvas = createCanvas(300, 300);
 canvas.center();
 video = createCapture(VIDEO);
-video.size(300,00);
+video.size(300,300);
 video.hide();
 
 poseNet = ml5.poseNet(video, modelLoaded);
@@ -25,7 +28,9 @@ function modelLoaded() {
 function gotPoses(results) {
 
     if(results.length>0) {
-        console.log(results);
+        console.log(results); 
+        lips_x = results[0].pose.nose.x;
+        lips_y = results[0].pose.nose.y;
         console.log("nose x =" + results[0].pose.nose.x);
         console.log("nose y =" + results[0].pose.nose.y);
     }
@@ -33,6 +38,8 @@ function gotPoses(results) {
 
 function draw() {
     image(video,0,0,300,300);
+
+    image(lips,lips_x -15,lips_y +15,30,30);
 }
 
 function take_snapshot() {
